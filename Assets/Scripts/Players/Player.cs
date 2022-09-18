fa4a423cc58918208941b7f9570075c30c83acf4
Darren Perry <darrencperry@gmail.com>
2022-09-18 14:44:37 +0100

    - Add functionality to receive updates to player data
    - Add functionality to receive updates to player data
    - Update the player name in the UI
    - Update the player avatar in the scene

--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Born.InterviewTest.Network;
+using UnityEngine;
 
 namespace Born.InterviewTest.Players
 {
@@ -8,5 +9,39 @@ namespace Born.InterviewTest.Players
     public class Player : MonoBehaviour
     {
         [SerializeField] private Transform avatarRoot;
+        private Object[] avatarPrefabs;
+        private IPlayerInfoProvider playerInfoProvider;
+        private GameObject avatarInstance;
+
+        private void Awake()
+        {
+            playerInfoProvider = Runtime.App.Instance.PlayerInfoProvider;
+            playerInfoProvider.OnUpdateAvatar += UpdateAvatar;
+
+            avatarPrefabs = Resources.LoadAll("Prefabs", typeof(GameObject));
+        }
+
+        private void UpdateAvatar()
+        {
+            GameObject newAatarPrefab = null;
+            foreach(GameObject avatarPrefab in avatarPrefabs)
+            {
+                if(avatarPrefab.name.EndsWith(playerInfoProvider.AvatarId.ToString()))
+                {
+                    newAatarPrefab = avatarPrefab;
+                    break;
+                }
+            }
+            if (newAatarPrefab != null)
+            {
+                if (avatarInstance != null) Destroy(avatarInstance);
+                avatarInstance = Instantiate(newAatarPrefab, avatarRoot);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            playerInfoProvider.OnUpdateAvatar -= UpdateAvatar;
+        }
     }
 }
--- Assets/Scripts/Players/PlayerNameView.cs
+++ Assets/Scripts/Players/PlayerNameView.cs
@@ -1,17 +1,35 @@
-﻿using UnityEngine;
+﻿using Born.InterviewTest.Network;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Born.InterviewTest.Players
 {
+    /// <summary>
+    /// Represents the player name in the scene. Responsible for displaying the player name on the UI canvas.
+    /// </summary>
     [RequireComponent(typeof(Text))]
     public class PlayerNameView : MonoBehaviour
     {
+        private IPlayerInfoProvider playerInfoProvider;
         private Text nameText;
         
         private void Awake()
         {
+            playerInfoProvider = Runtime.App.Instance.PlayerInfoProvider;
             nameText = GetComponent<Text>();
             nameText.text = "";
+
+            playerInfoProvider.OnUpdateName += UpdateName;
+        }
+
+        private void UpdateName()
+        {
+            nameText.text = playerInfoProvider.PlayerName ?? "";
+        }
+
+        private void OnDestroy()
+        {
+            playerInfoProvider.OnUpdateName -= UpdateName;
         }
     }
 }
